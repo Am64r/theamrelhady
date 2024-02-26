0888d96dbce3d91c61e315f853f4307922524bba
Amr Elhady <theamrelhady@gmail.com>
2024-02-26 17:20:57 -0500

    changes

--- app.py
+++ app.py
@@ -4,10 +4,21 @@ import constants
 import warnings
 
 from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import UnstructuredFileLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain_core._api.deprecation import LangChainDeprecationWarning
 from langchain_community.llms import openai
 from langchain_community.chat_models import ChatOpenAI
+
+
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+
+from langchain_core.messages import HumanMessage, SystemMessage
 
 warnings.filterwarnings("ignore", category=LangChainDeprecationWarning)
 
@@ -23,15 +34,22 @@ def add_cors_headers(response):
     return response
 
 @app.route('/generate-text', methods=['POST'])
+
 def generate_text():
-    data = request.json
-    query = data['text']
+    if (i <= 3):
+        data = request.json
+        query = data['text']
 
-    loader = TextLoader("data/reference.txt")
-    index = VectorstoreIndexCreator().from_loaders([loader])
-    result = index.query(query, llm=ChatOpenAI())
+        loader = TextLoader("data/reference.txt")
+        loader2 = DirectoryLoader('data/')
 
-    return jsonify({'generatedText': result})
+        index = VectorstoreIndexCreator().from_loaders([loader2])
+
+        result = index.query(query, llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo",))
+        i+=1
+
+        return jsonify({'generatedText': result})
+
 
 @app.route('/')
 def index():
